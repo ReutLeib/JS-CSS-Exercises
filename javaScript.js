43cbcb946a51329efb84bfb7a0732c34c44ff0ee
ReutLeib <leibreut@mail.com>
2018-11-28 16:51:05 +0200

    table css+js

--- javaScript.js
+++ javaScript.js
@@ -49,25 +49,52 @@
 
 
 /*index2.html*/
-window.onload = addRowHandlers; 
+// window.onload = addRowHandlers; 
 
-function addRowHandlers() {
-    var table = document.getElementById("tableId"),
-    rows = table.getElementsByTagName("tr");
+// function addRowHandlers() {
+//     var table = document.getElementById("tableId");
+//     var rows = table.getElementsByTagName("tr");
 
-    for (i = 0; i < rows.length; i++) {
-      var currentRow = table.rows[i];
+//     for (i = 0; i < rows.length; i++) {
+//       var currentRow = table.rows[i];
 
-      var createClickHandler = (row)=> {
-        return ()=> {
-          var cell = row.getElementsByTagName("td")[0];
-          var id = cell.innerHTML;
-          alert("id:" + id);
-        };
-      };
-      currentRow.onclick = createClickHandler(currentRow);
-    }
-  }
+//       var createClickHandler = (row)=> {
+//         //   console.log(`row: ${row.}`);
+//         return ()=> {
+//           var cell = row.getElementsByTagName("td")[0];
+//           var id = cell.innerHTML;
+//           alert("id:" + id);
+//         };
+//       };
+//       currentRow.onclick = createClickHandler(currentRow);
+//     }
+//   }
+
+
+
+/*index2.html*/
+
+// window.onload = addRowHandlers; 
+
+// function addRowHandlers() {
+//     var table = document.getElementById("container"),
+//     rows = document.getElementsByTagName("ul");
+//       console.log("rows.length: " + rows.length);
+
+//     for (i = 0; i < rows.length; i++) {
+//       var currentRow = rows[i];
+//       console.log("CURR: " + currentRow);
+//       var createClickHandler = (row)=> {
+//         // console.log("ROW: " + row);
+//         return ()=> {
+//           var cell = row.getElementsByClassName("col")[0];
+//           var id = cell.innerHTML;
+//           alert("id:" + id);
+//         };
+//       };
+//       currentRow.onclick = createClickHandler(currentRow);
+//     }
+//   }
 
 /*////////////////////////////////////////////////////////////////*/
   
@@ -189,8 +216,28 @@ function addRowHandlers() {
 
 // /*////////////////////////////////////////////////////////////////*/
 
+/*index6.html  TABLE*/
 
-/*index6.html*/
+// var eventDiv = document.getElementsByClassName("col");
+// var innerTxt, count=0;
+
+// function createClickHandler(Text) {
+//         return ()=> {
+//           alert(Text);
+//         };
+//       };
+
+// for (var i = 0; i < eventDiv.length; i++) {
+//     innerTxt = eventDiv[i].innerText;
+//     eventDiv[i].addEventListener('click', createClickHandler(innerTxt), false);
+// }
+
+/*////////////////////////////////////////////////////////////////*/
+
+
+
+/*index7.html*/
+
 
 
 
